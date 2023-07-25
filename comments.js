ed74df823673e437aa2dc4fd4ae2cc5397f6c6f1
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-07-25 21:39:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,67 +1,13 @@
-// Create Web Server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Create Database
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json'); // db.json is the name of the database file
-const db = low(adapter);
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ comments: [] }).write();
-
-// Create a route for getting all comments
-app.get('/comments', (req, res) => {
-    const comments = db.get('comments').value();
-    res.json(comments);
-    }
-);
-
-// Create a route for getting a single comment
-app.get('/comments/:id', (req, res) => {
-    const comment = db.get('comments')
-        .find({ id: req.params.id })
-        .value();
-    res.json(comment);
-    }
-);
-
-// Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-```
-I am trying to understand the code above. I understand that the code is creating a web server and a database. I am trying to understand the code below:
-```
-// Create a route for getting all comments
-app.get('/comments', (req, res) => {
-    const comments = db.get('comments').value();
-    res.json(comments);
-    }
-);
-```
-I understand that the code above is creating a route for getting all comments. I am trying to understand the code below:
-```
-const comments = db.get('comments').value();
-```
-
-What is the purpose of the code above?
-What is the purpose of the code below?
-```
-res.json(comments);
-```
+// Create a Web Server with Node.js
+// Path: server.js
+// const http = require('http');
+// const server = http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('<h1>Hello World</h1>');
+// });
+// server.listen(3000, '
+//     console.log('Server is running on port 3000');
+// ');
 
 
 
-I am trying to understand the code below:
-```
-// Create a route for getting a single comment
-app.get('/comments/:id', (req, res) => {
-
-    const comment = db.get('comments')
-        .find({ id: req.params.id })
-        .value();
-
-    res.json(comment);
-    }
-);
